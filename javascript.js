0d50a9f0cc46ceb7239566623c866a9f3bb5c93b
Brian Clark <bgclark@nic.edu>
2023-09-26 16:19:09 -0700

    initial commit

---
+++ javascript.js
@@ -0,0 +1,13 @@
+// 1.1 Assignment: Where do you put JavaScript?
+
+// 1.2 Assignment: Variables
+
+// 1.3 Assignment: Variable Declaration & Initialization
+
+// 1.4 Assignment: Complete your statements
+
+// 1.5 Assignment: Brackets, Braces and Parentheses
+
+// 1.6 Assignment: Data Types
+
+// 1.7 Assignment: Practicing JavaScript basics
